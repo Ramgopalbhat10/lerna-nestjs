5cfbfcb0ea3b1e17021b624e5e2a5839d7c1c784
Subbu <ramgopalbhat10@gmail.com>
2022-03-20 19:52:29 +0530

    added gateway auth

---
+++ packages/grpc-api-gateway/src/auth/auth.guard.ts
@@ -0,0 +1,40 @@
+/* eslint-disable prettier/prettier */
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpStatus,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ValidateResponse } from './auth.pb';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  @Inject(AuthService)
+  public readonly service: AuthService;
+
+  public async canActivate(ctx: ExecutionContext): Promise<boolean> | never {
+    const req: Request = ctx.switchToHttp().getRequest();
+    const authorization: string = req.headers['authorization'];
+
+    if (!authorization) {
+      throw new UnauthorizedException();
+    }
+
+    const bearer: string[] = authorization.split(' ');
+    if (!bearer || bearer.length < 2) {
+      throw new UnauthorizedException();
+    }
+
+    const token: string = bearer[1];
+    const { status }: ValidateResponse = await this.service.validate(token);
+
+    if (status !== HttpStatus.OK) {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+}
